0d8e8fad225221c9317ba568b9513f0e4c14b4b6
Shayan <shayanghodos@gmail.com>
2024-01-19 16:23:00 +0330

    change urls v2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('taxi.urls')),
+    path('', include('taxi.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path("__debug__/", include("debug_toolbar.urls")),
