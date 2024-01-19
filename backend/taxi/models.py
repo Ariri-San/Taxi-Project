118ee12205f26d8f30b0a2824073c7472a1982af
Shayan <shayanghodos@gmail.com>
2024-01-19 14:37:36 +0330

    models

--- backend/taxi/models.py
+++ backend/taxi/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
 
+
 class Price_Mile(models.Model):
-    name = models.CharField(_(""), max_length=50)
+    name = models.CharField(_("Name"), max_length=50)
