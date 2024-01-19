42f01034798cb8e33c618b7603f49d9d1b2865cb
IArshia <arshiapiryaee@yahoo.com>
2024-01-19 15:23:36 +0330

    models to fixed price

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # Django:
+
 ## Models:
-	Users (username, password, email)
-	Price Mile (7 days, name)
-  	Price Day (start, finish, price)
-	Fixed Price (destination, price)
-	Travel (user, price , date, date_return, origin, destination, payment_status, travel_Code)
-	History (user, price , date, date_return, origin, destination, payment_status, travel_Code)
+
+    Users (username, password, email)
+    Price Mile (name)
+
+Price Day (start, finish, price)
+JoinedPrice(day of week, priceday, pricemile)
+Fixed Price (destination, price)
+Travel (user, price , date, date_return, origin, destination, payment_status, travel_Code)
+History (user, price , date, date_return, origin, destination, payment_status, travel_Code)
 
 ## Views:
-	Home
-	Login
-	Register
-	Travel
-	Payment
-	Booked Travels
-	
-	Admin Travel
-	Admin History
+
+    Home
+    Login
+    Register
+    Travel
+    Payment
+    Booked Travels
+
+    Admin Travel
+    Admin History
 
 EMAIL
--- backend/taxi/models.py
+++ backend/taxi/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+from django.conf import settings
 
 # Create your models here.
 
-
-
-
-class Price_Mile(models.Model):
-    days = [""]
+class PriceMile(models.Model):
     name = models.CharField(_("Name"), max_length=50)
-    days = models.Choices()
-    MEMBERSHIP_ARTIST = 'A'
-    MEMBERSHIP_CUSTOMER = 'C'
-
-    MEMBERSHIP_CHOICES = [
-        (MEMBERSHIP_ARTIST, 'Artist'),
-        (MEMBERSHIP_CUSTOMER, 'Customer'),
-    ]
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_CUSTOMER)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
-
-
-
-class Price_Day(models.Model):
+class PriceDay(models.Model):
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     satrt = models.DateField()
+    finish = models.DateField()
+
+class JoinedPrice(models.Model):
+    SUNDAY = 'S'
+    OTHERDAYS = 'O'
+
+    DAY_CHOICES = [
+    (SUNDAY, "Sunday"),
+    (OTHERDAYS, "Otherdays"),
+    ]
+
+    day_of_week = models.Choices(max_length=1, choices=DAY_CHOICES)
+    priceday = models.ForeignKey(PriceDay, on_delete=models.CASCADE)
+    pricemile = models.ForeignKey(PriceMile, on_delete=models.CASCADE)
+
+class FixedPrice(models.Model):
+    price = models.DecimalField(max_digits=6, decimal_places=2)  
+    destination = models.CharField(max_length=255)
