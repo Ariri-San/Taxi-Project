d895d931bc32f7d93e4aa388e6ea6382b4fa42e6
Shayan <shayanghodos@gmail.com>
2024-01-19 14:31:36 +0330

    change name email user

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 class User(AbstractUser):
-    email = models.EmailField()
+    email = models.EmailField(_("email address"))
 
--- backend/taxi/models.py
+++ backend/taxi/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 # Create your models here.
 
+class Price_Mile(models.Model):
+    name = models.CharField(_(""), max_length=50)
