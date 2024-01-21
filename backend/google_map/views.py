2f1b27c49bb1dc9b7607c476ebb0cdd88d1c7dac
Shayan <shayanghodos@gmail.com>
2024-01-21 11:07:05 +0330

    add env

--- backend/google_map/views.py
+++ backend/google_map/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import googlemaps
+import os
+import environ
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+env = environ.Env()
+environ.Env.read_env()
+
+key = os.environ['KEY']
+gmaps = googlemaps.Client(key=key)
+
+@api_view(['POST'])
+def find_place(request):
+    name_search = request.data["name"]
+    
+    find_places = gmaps.find_place((name_search + ", UK"), "textquery")
+    
+    return Response("sdf", status=status.HTTP_200_OK)
