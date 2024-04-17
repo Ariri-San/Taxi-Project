4147a1e7ac36d3327fcf737f8b0fa45c40927891
Shayan <shayanghodos@gmail.com>
2024-04-17 11:37:32 +0330

    add hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['168.119.251.176', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['168.119.251.176', '127.0.0.1', 'localhost', 'welwynairporttaxiltd.com']
 
 INTERNAL_IPS = [
     # ...
