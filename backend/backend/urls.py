d177196fcc0dc7448aee4c6493bc80d09075d217
Shayan <shayanghodos@gmail.com>
2024-06-25 16:17:54 +0330

    add urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -25,3 +27,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    # urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
