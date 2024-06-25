c4e55cdce374b01825dfd408d84ca4388163354b
Shayan <shayanghodos@gmail.com>
2024-06-26 01:30:26 +0330

    debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-e7pi&mg-=@g$c0313k*%cs&-qmp$(cy0^c%ynsp5#ts$lbwvdl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shayan-projects.liara.run']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
